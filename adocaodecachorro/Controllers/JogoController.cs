c8087f4b3100c35a7ee68fca1b7686a7e34b4065
LabInfo <LabInfo@TERREOA2M09>
2023-10-04 20:33:09 -0300

    Catalogo de Jogos😍

---
+++ adocaodecachorro/Controllers/JogoController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CatalogoDeJogos.Models;
+
+namespace CatalogoDeJogos.Controllers
+{
+    public class JogoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View(JogoModel.CriarLista());
+        }
+
+
+        // GET: Aluno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // GET: Aluno/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(JogoModel.CriarLista()[id]);
+        }
+        
+        // GET: Aluno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(JogoModel.CriarLista()[id]);
+        }
+
+        // GET: Aluno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jogo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: Aluno/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+
+        // POST: Aluno/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ adocaodecachorro/Models/JogoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoDeJogos.Models
+{
+    public class JogoModel
+    {
+        public string GameID { get; set; }
+        public string Genero { get; set; }
+        public string Plataforma { get; set; }
+        public int AnoLancamento { get; set; }
+        public bool GOT { get; set; }
+      
+        public static List<JogoModel> CriarLista()
+        {
+            var lista = new List<JogoModel>();
+
+            lista.Add(new JogoModel() { GameID = "Elder Ring", Genero = "Aventura", AnoLancamento = 2022, GOT = true});
+            lista.Add(new JogoModel() { GameID = "The Last Of Us Part II", Genero = "Suspense", AnoLancamento = 2020, GOT = true});
+
+            return lista;
+        }
+    }
+
+}
